fa7f2c86a0c4fcaf1c4eb3d9a4d82aacf0da3cda
JustDoIt <59229675+Ccoderguy@users.noreply.github.com>
2021-04-18 15:58:56 -0300

    Add files via upload

---
+++ signup.inc.php
@@ -0,0 +1,43 @@
+<?php
+include "../utility/errorHandler.php";
+
+require_once "dbh.inc.php";
+require_once "functions.inc.php";
+
+/* Verify if the user entered this on the correct way (finishing form and pressing the button. */
+if (isset($_POST["submit"])){
+    echo "Tá serto";
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $username = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $pwdRepeat = $_POST["pwd2"];
+
+    if (emptyInputSignup($name, $email, $username, $pwd, $pwdRepeat) !== false){
+        header("location: ../signup.php?error=emptyinput");
+        exit();
+    }
+    if (invalidUid($username) !== false){
+        header("location: ../signup.php?error=invaliduid");
+        exit();
+    }
+    if (invalidEmail($email) !== false){
+        header("location: ../signup.php?error=invalidemail");
+        exit();
+    }
+    if (pwdMatch($pwd, $pwdRepeat) !== false){
+        header("location: ../signup.php?error=passworddontmatch");
+        exit();
+    }
+    if (uidExists($conn, $username, $email) !== false){
+        header("location: ../signup.php?error=usernametaken");
+        exit();
+    }
+
+    createUser($conn, $name, $email, $username, $pwd);
+
+} else {
+    echo "<script>errorPostSignup()</script>";
+    exit();
+}
